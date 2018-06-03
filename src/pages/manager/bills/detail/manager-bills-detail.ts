186ab18234098032cf0693009c37e42e08961eb1
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-06-03 19:41:40 -0300

    Manager bill detail page added

--- src/pages/manager/bills/detail/manager-bills-detail.ts
+++ src/pages/manager/bills/detail/manager-bills-detail.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+    ActionSheetController, AlertController, IonicPage, ModalController, NavController,
+    NavParams
+} from 'ionic-angular';
 import { ApiProvider } from "../../../../providers/api/api";
 
 @IonicPage()
@@ -8,12 +11,14 @@ import { ApiProvider } from "../../../../providers/api/api";
     templateUrl: 'manager-bills-detail.html',
 })
 export class ManagerBillsDetailPage {
-    private title = 'Comanda #';
+    private title;
     private bill;
     private id;
+    private loaded;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private apiProvider: ApiProvider,
-                public alertCtrl: AlertController) {
+    constructor(private navCtrl: NavController, private navParams: NavParams, private apiProvider: ApiProvider,
+                private alertCtrl: AlertController, private actionSheetCtrl: ActionSheetController,
+                private modalCtrl: ModalController) {
         this.id = this.navParams.get('id');
     }
 
@@ -21,10 +26,13 @@ export class ManagerBillsDetailPage {
      * Loads the bill data
      */
     ionViewWillEnter() {
+        this.title = 'Comanda #';
+
         if (this.id) {
             this.apiProvider.builder('bills/' + this.id).loader().get().subscribe(res => {
                 this.bill = res;
                 this.title = this.title + res.card.number;
+                this.loaded = true;
             });
         } else {
             this.navCtrl.setRoot('ManagerBillsListPage');
@@ -40,6 +48,10 @@ export class ManagerBillsDetailPage {
         this.navCtrl.push('ManagerBillsFormPage', {id: id});
     }
 
+    /**
+     *
+     * @param {number} id
+     */
     delete(id: number) {
         const confirm = this.alertCtrl.create({
             title: 'Excluir comanda',
@@ -63,5 +75,68 @@ export class ManagerBillsDetailPage {
         });
 
         confirm.present();
+    }
+
+    /**
+     *
+     * @param {number} id
+     * @param {number} key
+     */
+    showOptions(id: number, key: number) {
+        let actionSheet = this.actionSheetCtrl.create({
+            title: 'Opções',
+            buttons: [
+                {
+                    text: 'Editar',
+                    handler: () => {
+                        // let productModal = this.modalCtrl.create(OrderProductModalPage, {product: this.order[key]});
+                        //
+                        // productModal.onDidDismiss(data => {
+                        //     if (data instanceof Product) {
+                        //         this.order[key] = data;
+                        //         this.updateTotal();
+                        //     }
+                        // });
+                        //
+                        // productModal.present();
+                    }
+                },
+                {
+                    text: 'Remover',
+                    role: 'destructive',
+                    handler: () => {
+                        let alert = this.alertCtrl.create({
+                            title: 'Confirmar exclusão',
+                            message: 'Deseja remover esse produto da comanda?',
+                            buttons: [
+                                {
+                                    text: 'Não',
+                                    role: 'cancel'
+                                },
+                                {
+                                    text: 'Sim',
+                                    handler: () => {
+                                        this.removeProduct(id);
+                                    }
+                                }
+                            ]
+                        });
+
+                        alert.present();
+                    }
+                },
+                {
+                    text: 'Cancelar',
+                    role: 'cancel'
+                }
+            ]
+        });
+
+        actionSheet.present();
+    }
+
+    //
+    removeProduct(id: number) {
+
     }
 }
