4ee8ea1a085fe05553ed457e22fb2e3f196cc5c5
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-06-08 17:55:40 -0300

    Expandable header added

--- src/pages/default/products/index/products.module.ts
+++ src/pages/default/products/index/products.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProductsPage } from './products';
+import { ExpandableHeaderComponent } from "../../../../components/expandable-header/expandable-header";
 
 @NgModule({
     declarations: [
         ProductsPage,
+        ExpandableHeaderComponent
     ],
     imports: [
         IonicPageModule.forChild(ProductsPage),
