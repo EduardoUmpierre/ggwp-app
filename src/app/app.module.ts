aec17282edde818bea155f5b15822caa4656362d
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-06-11 00:00:11 -0300

    Components module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { HttpNativeProvider } from "../providers/api/http/http-native";
 import { HTTP } from "@ionic-native/http";
 import { BrMaskerModule } from "brmasker-ionic-3";
 import { SelectSearchableModule } from 'ionic-select-searchable';
+import { IonicStorageModule } from "@ionic/storage";
+import { AuthProvider } from "../providers/auth/auth";
+import { ComponentsModule } from "../components/components.module";
 
 @NgModule({
     declarations: [
@@ -26,8 +29,13 @@ import { SelectSearchableModule } from 'ionic-select-searchable';
         HttpClientModule,
         BrMaskerModule,
         SelectSearchableModule,
+        ComponentsModule,
         IonicModule.forRoot(MyApp, {
             tabsPlacement: 'top'
+        }),
+        IonicStorageModule.forRoot({
+            name: '__mydb',
+            driverOrder: ['indexeddb', 'sqlite', 'websql']
         })
     ],
     bootstrap: [IonicApp],
@@ -39,6 +47,7 @@ import { SelectSearchableModule } from 'ionic-select-searchable';
         StatusBar,
         SplashScreen,
         ApiProvider,
+        AuthProvider,
         HttpProvider,
         HttpAngularProvider,
         HttpNativeProvider,
