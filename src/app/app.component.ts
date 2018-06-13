ba2cf23508392bb75cd91756e36c68eea302da58
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-06-13 18:03:18 -0300

    Main menu added to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,40 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Events, MenuController, ModalController, Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/default/tabs/tabs';
+import { Storage } from "@ionic/storage";
+import { AuthProvider } from "../providers/auth/auth";
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
+    private enabledMenu = 'main-menu';
+    private disabledMenu = 'manager-menu';
+    private user;
+
     @ViewChild(Nav) nav: Nav;
 
     rootPage: any = TabsPage;
+
     // rootPage: any = 'ManagerProductsListPage';
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private events: Events,
+                private storage: Storage, private menu: MenuController, private authProvider: AuthProvider,
+                private modalCtrl: ModalController) {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             statusBar.styleDefault();
             splashScreen.hide();
+        });
+
+        this.updateActiveMenu();
+
+        events.subscribe('user:updated', (user, time) => {
+            this.updateActiveMenu();
         });
     }
 
@@ -28,5 +43,49 @@ export class MyApp {
      */
     openPage(page) {
         this.nav.setRoot(page);
+    }
+
+    /**
+     * Updates the main menu by user role
+     */
+    updateActiveMenu() {
+        this.storage.get('user').then(user => {
+            this.user = false;
+            this.enabledMenu = 'main-menu';
+            this.disabledMenu = 'manager-menu';
+
+            if (user) {
+                this.user = true;
+
+                if (user.role && user.role > 1) {
+                    this.enabledMenu = 'manager-menu';
+                    this.disabledMenu = 'main-menu';
+                }
+            }
+
+            this.menu.enable(true, this.enabledMenu);
+            this.menu.enable(false, this.disabledMenu);
+
+            console.log('Menu ativado', this.enabledMenu);
+            console.log('Menu desativado', this.disabledMenu);
+        })
+    }
+
+    /**
+     * Logout
+     */
+    logout() {
+        this.authProvider.loader('Saindo').logout().then(() => {
+            this.authProvider.hideLoader();
+            this.events.publish('user:updated', true);
+        });
+    }
+
+    /**
+     * Calls the login page
+     */
+    login() {
+        const modal = this.modalCtrl.create('LoginPage');
+        modal.present();
     }
 }
--- src/components/expandable-header/expandable-header.ts
+++ src/components/expandable-header/expandable-header.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ElementRef, Renderer2 } from '@angular/core';
-import { Storage } from "@ionic/storage";
-import { ModalController } from "ionic-angular";
+import { Storage } from '@ionic/storage';
+import { ModalController, Events } from 'ionic-angular';
 
 @Component({
     selector: 'expandable-header',
@@ -10,11 +10,14 @@ export class ExpandableHeaderComponent {
     @Input('scrollArea') scrollArea: any;
     @Input('headerHeight') headerHeight: number;
 
-    newHeaderHeight: any;
+    private newHeaderHeight: any;
     private user = null;
 
     constructor(private element: ElementRef, private renderer: Renderer2, private storage: Storage,
-                private modalCtrl: ModalController) {
+                private modalCtrl: ModalController, private events: Events) {
+        events.subscribe('user:updated', (user) => {
+            this.updateUserData();
+        });
     }
 
     ngOnInit() {
@@ -23,13 +26,21 @@ export class ExpandableHeaderComponent {
         this.scrollArea.ionScroll.subscribe((ev) => {
             this.resizeHeader(ev);
         });
-
-        setTimeout(() => {
-            this.renderer.setStyle(this.scrollArea._elementRef.nativeElement.firstChild, 'margin-top', '90px');
-        }, 300);
     }
 
     ngOnChanges() {
+        this.updateUserData();
+    }
+
+    ionViewWillLeave() {
+        this.events.unsubscribe('user:updated');
+        console.log('unsubcribed');
+    }
+
+    /**
+     * Updates the user data from local storage
+     */
+    private updateUserData() {
         this.storage.get('user').then(user => this.user = user);
     }
 
@@ -41,6 +52,9 @@ export class ExpandableHeaderComponent {
         modal.present();
     }
 
+    /**
+     * Calls the profile page
+     */
     goToProfile() {
         const modal = this.modalCtrl.create('ProfilePage', {user: this.user});
         modal.present();
@@ -53,29 +67,39 @@ export class ExpandableHeaderComponent {
      */
     resizeHeader(ev) {
         ev.domWrite(() => {
-            this.newHeaderHeight = this.headerHeight - ev.scrollTop;
+            this.newHeaderHeight = this.headerHeight - Math.floor(ev.scrollTop * 0.5);
 
             if (this.newHeaderHeight < 0) {
                 this.newHeaderHeight = 0;
             }
 
-            const contentElementRef = this.scrollArea._elementRef.nativeElement;
-            const contentTabsRef = this.scrollArea._tabs;
-            let tabHeight = this.newHeaderHeight;
-            let tabRefHeight = tabHeight;
-            const tabYMargin = tabHeight - this.headerHeight;
+            if (this.newHeaderHeight > 0) {
+                const contentElementRef = this.scrollArea._elementRef.nativeElement;
+                const contentTabsRef = this.scrollArea._tabs;
+                let tabRefHeight = this.newHeaderHeight;
+                const tabYMargin = this.newHeaderHeight - this.headerHeight;
 
-            if (contentTabsRef) {
-                this.renderer.setStyle(contentTabsRef._tabbar.nativeElement, 'transform', 'translate3d(0, ' + tabYMargin + 'px, 0)');
+                if (contentTabsRef) {
+                    this.renderer.setStyle(contentTabsRef._tabbar.nativeElement, 'transform', 'translate3d(0, ' + tabYMargin + 'px, 0)');
 
-                tabRefHeight = tabHeight + 56;
+                    tabRefHeight = this.newHeaderHeight + 56;
+                }
+
+                const scrollContent = tabRefHeight;
+
+                // this.renderer.setStyle(this.element.nativeElement, 'height', tabHeight + 'px');
+                this.renderer.setStyle(contentElementRef.lastChild, 'margin-top', scrollContent + 'px');
+                this.renderer.setStyle(document.querySelector('#user-header.compact'), 'opacity', this.map(this.newHeaderHeight, 30, 90, 0, 100)/100);
+                // this.element.nativeElement.children[0]
+
+                console.log(this.map(this.newHeaderHeight, 30, 90, 0, 100)/100);
             }
-
-            const scrollContent = tabRefHeight;
-
-            this.renderer.setStyle(this.element.nativeElement, 'height', tabHeight + 'px');
-            this.renderer.setStyle(contentElementRef.lastChild, 'margin-top', scrollContent + 'px');
-            this.renderer.setStyle(this.element.nativeElement.children[0], 'opacity', (tabHeight + 10) / 100);
         });
+    }
+
+    private map(num, in_min, in_max, out_min, out_max) {
+        let result = (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+        return result < 0 ? 0 : result;
     }
 }
