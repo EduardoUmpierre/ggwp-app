12e88d8931728419588fe4054a1fd526dd89e358
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-19 13:04:00 -0300

    User achievements, drops and badges list added

--- src/pages/default/profile/profile.ts
+++ src/pages/default/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, ViewController } from 'ionic-angular';
-import { Storage } from "@ionic/storage";
+import { Storage } from '@ionic/storage';
+import { ApiProvider } from '../../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -9,15 +10,33 @@ import { Storage } from "@ionic/storage";
 })
 export class ProfilePage {
     private user;
+    private achievements;
+    private badges;
+    private drops;
 
-    constructor(private storage: Storage, private viewCtrl: ViewController) {
+    constructor(private storage: Storage, private apiProvider: ApiProvider, private viewCtrl: ViewController) {
     }
 
     /**
      * Loads the user data
      */
     ionViewWillEnter() {
-        this.storage.get('user').then((user) => this.user = user);
+        this.storage.get('user').then((user) => {
+            this.user = user;
+
+            this.apiProvider.builder(`users/${user.id}/badges`).get().subscribe(badges => {
+                this.badges = badges;
+
+                this.apiProvider.builder(`users/${user.id}/drops`).get().subscribe(drops => {
+                    this.drops = drops;
+
+                    this.apiProvider.builder(`users/${user.id}/achievements`).get().subscribe(achievements => {
+                        this.achievements = achievements;
+                    });
+                });
+            });
+
+        });
     }
 
     /**
